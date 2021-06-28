0b838d53694b3611197ce773d9f8c0fe6bc56a85
LiquiDAO Developers <74860369+liquidao-dev@users.noreply.github.com>
2021-06-28 12:47:27 +0000

    Lint & Publish GH Action (#1)
    * Lint & Publish GH Action
    
    * add check in lint gh action
    
    * remove build from git
    
    * Fux typo

--- src/pages/Pools/Pools.js
+++ src/pages/Pools/Pools.js
@@ -1,81 +1,74 @@
 import React from 'react';
 import './Pools.css';
-function Pools () {
-    return (
-        <div className="Pools">
-            <div className="section-pools">
-                <div className="header-pools">
-                    <h2>All pools</h2>
-                </div>
-
-                <table>
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Pool</th>
-                        <th>TVL</th>
-                        <th>Volume 24H</th>
-                        <th>Volume 7D</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    </tbody>
-                </table>
-                <div className="pagination">
-
-                </div>
-                <div className="btn-block">
-                    <button  className="btn-add">
-                        Add Luquidity
-                    </button>
-                </div>
-
-            </div>
-
-
+function Pools() {
+  return (
+    <div className="Pools">
+      <div className="section-pools">
+        <div className="header-pools">
+          <h2>All pools</h2>
         </div>
-    )
+
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Pool</th>
+              <th>TVL</th>
+              <th>Volume 24H</th>
+              <th>Volume 7D</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="pagination"></div>
+        <div className="btn-block">
+          <button className="btn-add">Add Liquidity</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Pools;
