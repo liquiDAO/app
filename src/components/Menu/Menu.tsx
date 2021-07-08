fab148642e5fa1d67c42b7ebd1a1255727d7aa74
kosidev <kosiso@kosidev.com>
2021-07-09 02:17:04 +0400

    refactore: changed menu component to TypeScript

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import './Menu.css';
 import { link } from '../../utils';
 
-function Menu({ openModalStake, isInstalled, isConnected }) {
-
-  return (
+export const Menu: React.FC = ({ openModalStake, isInstalled, isConnected }: any) => {
+    return (
     <div className="Menu">
       <div className="actions">
         <label className="switch">
@@ -12,9 +11,8 @@ function Menu({ openModalStake, isInstalled, isConnected }) {
           <span className="slider round" />
         </label>
         <p onClick={openModalStake}>STAKE</p>
-        <button id="btn1" style={{display: isConnected ? 'none': null}} onClick={isInstalled === false && link}>{ isInstalled ? 'Connect wallet' : 'Install Marina wallet' }</button>
+        <button id="btn1" style={{display: isConnected ? 'none': 'block'}} onClick={() => isInstalled === false && link}>{ isInstalled ? 'Connect wallet' : 'Install Marina wallet' }</button>
       </div>
     </div>
   );
 }
-export default Menu;
