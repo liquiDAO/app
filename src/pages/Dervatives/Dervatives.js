5741863526271deb6f389fa34edfabac8af0d251
kosidocs <kelvinonaru@gmail.com>
2021-07-09 04:16:54 +0400

    refactore: changed Dervatives page to typescript

--- src/pages/Dervatives/Dervatives.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Dervatives.css';
-function Dervatives() {
-  return <div className="Dervatives">Dervatives</div>;
-}
-export default Dervatives;
---
+++ src/pages/Dervatives/Dervatives.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Dervatives.css';
+
+const Dervatives: React.FC = () => {
+  return (<div className="Dervatives">Dervatives</div>);
+}
+export default Dervatives;
