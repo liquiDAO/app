5bf607ffaab51ee85f0fe9f434a5d934db87b6f6
kosiso. O <kelvinonaru@gmail.com>
2021-07-19 15:19:56 +0400

    fix: Swap layout

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Swap.css';
 import { handleInstall } from '../../utils';
 
@@ -8,6 +8,7 @@ interface SwapProp {
   checkCoinBottom: any;
   isInstalled: boolean;
   isConnected: boolean;
+  changeToken: any;
 }
 
 export const Swap: React.FC<SwapProp> = ({
@@ -16,7 +17,12 @@ export const Swap: React.FC<SwapProp> = ({
   checkCoinBottom,
   isInstalled,
   isConnected,
+  changeToken,
 }) => {
+  const [changeFlag, setChangeFlag] = useState<boolean>(false);
+  const [amountToBeSent, setAmountToBeSent] = useState<string>('0.0');
+  const [amountToReceive, setAmountToReceive] = useState<string>('0.0');
+
   const handleConnect = async () => {
     if (!isInstalled) {
       return alert('Marina is not installed');
@@ -37,50 +43,53 @@ export const Swap: React.FC<SwapProp> = ({
 
   return (
     <div className="Swap">
-      <div className="section-swap">
-        <div className="header">
-          <h2>SWAP</h2>
-          <img src="/images/iconfinder_icons_settings_1564529 1.png" alt="" />
-        </div>
+           <div className="section-swap">
+                <div className="header">
+                    <h2>SWAP</h2>
+                    <img src="/images/iconfinder_icons_settings_1564529 1.png" alt=""/>
+                </div>
 
-        <div className="change">
-          <div className="top" onClick={() => selectToken('top')}>
-            <div className="select">
-              <span>ETH</span>
-              <img
-                src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
-                alt=""
-              />
-            </div>
-            <div className="count">
-              <div className="selectedCoin">
-                <img src={`/images/${checkedCoin.image}`} alt="" />
-              </div>
-              0.0
-            </div>
-            <div
-              className="convert"
-              onClick={(event) => event.stopPropagation()}
-            >
-              <img src="/images/Vector.png" alt="" />
-            </div>
-          </div>
+               <div className="change">
+                   <div className="top">
+                        <div className="select" onClick={() => {selectToken('top')}}>
+                            <div className="selectedCoin">
+                <img src={`/images/${checkedCoin.label}`} alt=""/>
+                            </div>
+                            <span>{checkedCoin && checkedCoin.title}</span>
+                            <img src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png" alt=""/>
+                        </div>
+                       <div className="count">
+              <input type="text" placeholder='0.0' onChange={(evt) => {
+                setAmountToBeSent(evt.target.value);
+              }}/>
+                       </div>
+                       <div className="convert" onClick={(evt) => {
+                          let flag = !changeFlag;
+                           setChangeFlag(flag)
+                           changeToken(evt)
+                       }}>
+                           {
+                               changeFlag ?  <img src="/images/arow.png" alt=""  className="top-arow" /> : <img  src="/images/Vector.png" alt=""/>
+                           }
 
-          <div className="bottom">
-            <div className="select" onClick={() => selectToken('bottom')}>
-              <span>Select a token</span>
-              <img
-                src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
-                alt=""
-              />
-            </div>
-            <div className="count">
-              <div className="selectedCoin">
-                <img src={`/images/${checkCoinBottom.image}`} alt="" />
-              </div>
-              0.0
-            </div>
-          </div>
+                       </div>
+                   </div>
+
+                   <div className="bottom">
+                       <div className="select" onClick={() => selectToken('bottom')}>
+                           <div className="selectedCoin">
+                <img src={`/images/${checkCoinBottom.image}`} alt=""/>
+                
+                           </div>
+                           <span >{ checkCoinBottom && checkCoinBottom.title}</span>
+                           <img src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png" alt=""/>
+                       </div>
+                       <div className="count">
+              <input type="text" placeholder='0.0' onChange={(evt) => {
+                 setAmountToReceive(evt.target.value);
+              }}/>
+                       </div>
+                   </div>
           {isInstalled && isConnected ? (
             <button onClick={handleSwap} className="connect-wallet">
               Swap
