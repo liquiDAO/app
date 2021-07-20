efc256a7a188e1188d0961af7788f7ca898733b2
kosiso. O <kelvinonaru@gmail.com>
2021-07-20 19:48:08 +0400

    feat: add market price preview for both baseasset and qouteasset

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
+import { TradeType } from 'tdex-sdk';
+import BigNumber from 'bignumber.js';
 import './Swap.css';
 import { handleInstall } from '../../utils';
-import { handlePair } from '../../utils/TedexSetup';
+import { onSendAmountChange } from '../../utils/TedexSetup';
+
+enum Asset {
+  base = "5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225",
+  qoute = "9b3d7db7f93cae59e9407232b9864b54c21cba20b992d707fa45136fbfa9b8fa",
+}
 
 interface SwapProp {
   selectToken: any;
@@ -23,6 +30,7 @@ export const Swap: React.FC<SwapProp> = ({
   const [changeFlag, setChangeFlag] = useState<boolean>(false);
   const [amountToBeSent, setAmountToBeSent] = useState<string>('0.0');
   const [amountToReceive, setAmountToReceive] = useState<string>('0.0');
+  const [isPreviewing, setIsPreviewing] = useState<boolean>(false);
 
   const handleConnect = async () => {
     if (!isInstalled) {
@@ -40,60 +48,140 @@ export const Swap: React.FC<SwapProp> = ({
     if (!isConnected) {
       return alert('User must enable this website to proceed');
     }
-
-    console.log(amountToBeSent, amountToReceive);
-    handlePair();
   };
+
+  const amountIsPositive = (x: any): boolean => {
+    if (!Number.isNaN(x) && Number(x) > 0) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const handleBaseAsset = async (
+    evt: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+  ) => {
+    if (isPreviewing) return;
+
+    const value = evt.target.value;
+    setAmountToBeSent(value);
+
+    if (amountIsPositive(value)) {
+      setIsPreviewing(true);
+
+      const amount = Number(value);
+
+      try {
+        const toRecieve = await onSendAmountChange(amount, TradeType.SELL, Asset.base);
+        setAmountToReceive(convertAmountToString(toRecieve));
+        setIsPreviewing(false);
+      } catch (error) {
+        console.log('can not set amount to recieve', error);
+      }
+    }
+  };
+
+  const handleQouteAsset = async (
+    evt: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+  ) => {
+    if (isPreviewing) return;
+
+    const value = evt.target.value;
+    setAmountToReceive(value);
+
+    if (amountIsPositive(value)) {
+      setIsPreviewing(true);
+
+      const amount = Number(value);
+
+      try {
+        const toRecieve = await onSendAmountChange(amount, TradeType.BUY, Asset.qoute);
+        setAmountToBeSent(convertAmountToString(toRecieve));
+        setIsPreviewing(false);
+      } catch (error) {
+        console.log('can not set amount to recieve', error);
+      }
+    }
+  };
+
+  const convertAmountToString = (amount: BigNumber | null): string =>
+    amount!.toNumber().toLocaleString('en-US', {
+      maximumFractionDigits: 8,
+    });
 
   return (
     <div className="Swap">
-           <div className="section-swap">
-                <div className="header">
-                    <h2>SWAP</h2>
-                    <img src="/images/iconfinder_icons_settings_1564529 1.png" alt=""/>
-                </div>
+      <div className="section-swap">
+        <div className="header">
+          <h2>SWAP</h2>
+          <img src="/images/iconfinder_icons_settings_1564529 1.png" alt="" />
+        </div>
 
-               <div className="change">
-                   <div className="top">
-                        <div className="select" onClick={() => {selectToken('top')}}>
-                            <div className="selectedCoin">
-                <img src={`/images/${checkedCoin.image}`} alt=""/>
-                            </div>
-                            <span>{checkedCoin && checkedCoin.title}</span>
-                            <img src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png" alt=""/>
-                        </div>
-                       <div className="count">
-              <input type="text" placeholder='0.0' onChange={(evt) => {
-                setAmountToBeSent(evt.target.value);
-              }}/>
-                       </div>
-                       <div className="convert" onClick={(evt) => {
-                          let flag = !changeFlag;
-                           setChangeFlag(flag)
-                           changeToken(evt)
-                       }}>
-                           {
-                               changeFlag ?  <img src="/images/arow.png" alt=""  className="top-arow" /> : <img  src="/images/Vector.png" alt=""/>
-                           }
+        <div className="change">
+          <div className="top">
+            <div
+              className="select"
+              onClick={() => {
+                selectToken('top');
+              }}
+            >
+              <div className="selectedCoin">
+                <img src={`/images/${checkedCoin.image}`} alt="" />
+              </div>
+              <span>{checkedCoin && checkedCoin.title}</span>
+              <img
+                src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
+                alt=""
+              />
+            </div>
+            <div className="count">
+              <input
+                type="text"
+                placeholder="0.0"
+                value={amountToBeSent}
+                onChange={(evt) => {
+                  handleBaseAsset(evt);
+                }}
+              />
+            </div>
+            <div
+              className="convert"
+              onClick={(evt) => {
+                let flag = !changeFlag;
+                setChangeFlag(flag);
+                changeToken(evt);
+              }}
+            >
+              {changeFlag ? (
+                <img src="/images/arow.png" alt="" className="top-arow" />
+              ) : (
+                <img src="/images/Vector.png" alt="" />
+              )}
+            </div>
+          </div>
 
-                       </div>
-                   </div>
-
-                   <div className="bottom">
-                       <div className="select" onClick={() => selectToken('bottom')}>
-                           <div className="selectedCoin">
-                <img src={`/images/${checkCoinBottom.image}`} alt=""/>
-                
-                           </div>
-                           <span >{ checkCoinBottom && checkCoinBottom.title}</span>
-                           <img src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png" alt=""/>
-                       </div>
-                       <div className="count">
-              <input type="text" placeholder='0.0' onChange={(evt) => {
-                 setAmountToReceive(evt.target.value);
-              }}/>
-                       </div>
-                   </div>
+          <div className="bottom">
+            <div className="select" onClick={() => selectToken('bottom')}>
+              <div className="selectedCoin">
+                <img src={`/images/${checkCoinBottom.image}`} alt="" />
+              </div>
+              <span>{checkCoinBottom && checkCoinBottom.title}</span>
+              <img
+                src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
+                alt=""
+              />
+            </div>
+            <div className="count">
+              <input
+                type="text"
+                placeholder="0.0"
+                value={amountToReceive}
+                onChange={(evt) => {
+                  handleQouteAsset(evt);
+                }}
+              />
+            </div>
+          </div>
           {isInstalled && isConnected ? (
             <button onClick={handleSwap} className="connect-wallet">
               Swap
