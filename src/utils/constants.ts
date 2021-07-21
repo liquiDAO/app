a3643b6fa666734ea8b48ba2c3d758590f11b497
kosiso. O <kelvinonaru@gmail.com>
2021-07-21 09:01:11 +0400

    feat: add constant file for asset hash

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+export enum AssetHash {
+  BaseAsset = '5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225',
+  QuoteAsset = '9b3d7db7f93cae59e9407232b9864b54c21cba20b992d707fa45136fbfa9b8fa',
+}
