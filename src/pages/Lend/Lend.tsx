e3cd40758a5b48e85b5eb9a900ede4b8fae12483
kosiso. O <kelvinonaru@gmail.com>
2021-08-08 19:25:57 +0400

    bugfix: fixed layout issues

--- src/pages/Lend/Lend.tsx
+++ src/pages/Lend/Lend.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Mobile from '../NoMobileSupport/Mobile';
 import './Lend.css';
+import ComingSoon from '../ComingSoon/ComingSoon';
 
 interface LendProps {
   isMobile: boolean;
@@ -11,180 +12,181 @@ const Lend: React.FC<LendProps> = ({ isMobile }) => {
     return <Mobile />;
   }
   return (
-    <div className="Lend">
-      <div className="wrapper">
-        <h1 style={{ margin: '0px auto', textAlign: 'center', color: 'white' }}>
-          Coming Soon
-        </h1>
-        <img
-          src="/images/illustration2.svg"
-          style={{ width: '100%' }}
-          alt="Coming Soon"
-        />
-        {/* <div className="findOffers">
-          <h2>Find offers</h2>
-          <div className="chips-voting">
-            <div>
-              <input className="chip-voting" type="text" />
-            </div>
-            <div className="chip-voting">
-              BTC
-              <img
-                src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
-                alt=""
-              />
-            </div>
-            <div className="chip-voting">
-              from 30 %
-              <img
-                src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
-                alt=""
-              />
-            </div>
-            <div className="chip-voting">
-              to 80 %
-              <img
-                src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
-                alt=""
-              />
-            </div>
-            <div className="chip-voting">
-              from 1 day
-              <img
-                src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
-                alt=""
-              />
-            </div>
-            <div className="chip-voting">
-              to 12 months
-              <img
-                src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
-                alt=""
-              />
-            </div>
-          </div>
-          <button>FIND OFFERS</button>
-        </div>
-        <div className="section-lend">
-          <h2>To borrow offers</h2>
-          <button>CREATE OFFERS</button>
-        </div>
-        <div className="filter-lend">
-          Sort by
-          <img
-            src="/images/iconfinder_2738302_filter_settings_sliders_icon 1.png"
-            alt=""
-          />
-        </div>
+    <ComingSoon />
+    // <div className="Lend">
+    //   <div className="wrapper">
+    //     <h1 style={{ margin: '0px auto', textAlign: 'center', color: 'white' }}>
+    //       Coming Soon
+    //     </h1>
+    //     <img
+    //       src="/images/illustration2.svg"
+    //       style={{ width: '100%' }}
+    //       alt="Coming Soon"
+    //     />
+    //     <div className="findOffers">
+    //       <h2>Find offers</h2>
+    //       <div className="chips-voting">
+    //         <div>
+    //           <input className="chip-voting" type="text" />
+    //         </div>
+    //         <div className="chip-voting">
+    //           BTC
+    //           <img
+    //             src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div className="chip-voting">
+    //           from 30 %
+    //           <img
+    //             src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div className="chip-voting">
+    //           to 80 %
+    //           <img
+    //             src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div className="chip-voting">
+    //           from 1 day
+    //           <img
+    //             src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div className="chip-voting">
+    //           to 12 months
+    //           <img
+    //             src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
+    //             alt=""
+    //           />
+    //         </div>
+    //       </div>
+    //       <button>FIND OFFERS</button>
+    //     </div>
+    //     <div className="section-lend">
+    //       <h2>To borrow offers</h2>
+    //       <button>CREATE OFFERS</button>
+    //     </div>
+    //     <div className="filter-lend">
+    //       Sort by
+    //       <img
+    //         src="/images/iconfinder_2738302_filter_settings_sliders_icon 1.png"
+    //         alt=""
+    //       />
+    //     </div>
 
-        <div className="stakes-lend">
-          <div className="section-reward">
-            <div className="box-reward">
-              <div className="stakes-section-lend">
-                <div className="block-section">
-                  <h4>Borrow</h4>
-                  <p>from 1 day to 3 months</p>
-                </div>
-                <div className="block-section">
-                  <h4>Period</h4>
-                  <p>from 1 day to 3 months</p>
-                </div>
-                <div className="block-section">
-                  <h4>LTV ratio</h4>
-                  <p>80%</p>
-                </div>
-                <div className="block-section">
-                  <h4>Interest rate</h4>
-                  <p>1%</p>
-                </div>
-                <div className="block-section">
-                  <h4>APR</h4>
-                  <p>365% - 4%</p>
-                </div>
-                <div className="block-section">
-                  <h4>User</h4>
-                  <p>ObsidianCalyx (No rating)</p>
-                </div>
-              </div>
-              <div className="read-more">
-                <p>
-                  Сlick here for more information{' '}
-                  <img src="/images/Arrow 3.png" alt="" />
-                </p>
-              </div>
-            </div>
-            <div className="box-reward">
-              <div className="stakes-section-lend">
-                <div className="block-section">
-                  <h4>Borrow</h4>
-                  <p>from 1 day to 3 months</p>
-                </div>
-                <div className="block-section">
-                  <h4>Period</h4>
-                  <p>from 1 day to 3 months</p>
-                </div>
-                <div className="block-section">
-                  <h4>LTV ratio</h4>
-                  <p>80%</p>
-                </div>
-                <div className="block-section">
-                  <h4>Interest rate</h4>
-                  <p>1%</p>
-                </div>
-                <div className="block-section">
-                  <h4>APR</h4>
-                  <p>365% - 4%</p>
-                </div>
-                <div className="block-section">
-                  <h4>User</h4>
-                  <p>ObsidianCalyx (No rating)</p>
-                </div>
-              </div>
-              <div className="read-more">
-                <p>
-                  Сlick here for more information{' '}
-                  <img src="/images/Arrow 3.png" alt="" />
-                </p>
-              </div>
-            </div>
-            <div className="box-reward">
-              <div className="stakes-section-lend">
-                <div className="block-section">
-                  <h4>Borrow</h4>
-                  <p>from 1 day to 3 months</p>
-                </div>
-                <div className="block-section">
-                  <h4>Period</h4>
-                  <p>from 1 day to 3 months</p>
-                </div>
-                <div className="block-section">
-                  <h4>LTV ratio</h4>
-                  <p>80%</p>
-                </div>
-                <div className="block-section">
-                  <h4>Interest rate</h4>
-                  <p>1%</p>
-                </div>
-                <div className="block-section">
-                  <h4>APR</h4>
-                  <p>365% - 4%</p>
-                </div>
-                <div className="block-section">
-                  <h4>User</h4>
-                  <p>ObsidianCalyx (No rating)</p>
-                </div>
-              </div>
-              <div className="read-more">
-                <p>
-                  Сlick here for more information{' '}
-                  <img src="/images/Arrow 3.png" alt="" />
-                </p>
-              </div>
-            </div>
-          </div>
-        </div> */}
-      </div>
-    </div>
+    //     <div className="stakes-lend">
+    //       <div className="section-reward">
+    //         <div className="box-reward">
+    //           <div className="stakes-section-lend">
+    //             <div className="block-section">
+    //               <h4>Borrow</h4>
+    //               <p>from 1 day to 3 months</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>Period</h4>
+    //               <p>from 1 day to 3 months</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>LTV ratio</h4>
+    //               <p>80%</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>Interest rate</h4>
+    //               <p>1%</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>APR</h4>
+    //               <p>365% - 4%</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>User</h4>
+    //               <p>ObsidianCalyx (No rating)</p>
+    //             </div>
+    //           </div>
+    //           <div className="read-more">
+    //             <p>
+    //               Сlick here for more information{' '}
+    //               <img src="/images/Arrow 3.png" alt="" />
+    //             </p>
+    //           </div>
+    //         </div>
+    //         <div className="box-reward">
+    //           <div className="stakes-section-lend">
+    //             <div className="block-section">
+    //               <h4>Borrow</h4>
+    //               <p>from 1 day to 3 months</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>Period</h4>
+    //               <p>from 1 day to 3 months</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>LTV ratio</h4>
+    //               <p>80%</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>Interest rate</h4>
+    //               <p>1%</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>APR</h4>
+    //               <p>365% - 4%</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>User</h4>
+    //               <p>ObsidianCalyx (No rating)</p>
+    //             </div>
+    //           </div>
+    //           <div className="read-more">
+    //             <p>
+    //               Сlick here for more information{' '}
+    //               <img src="/images/Arrow 3.png" alt="" />
+    //             </p>
+    //           </div>
+    //         </div>
+    //         <div className="box-reward">
+    //           <div className="stakes-section-lend">
+    //             <div className="block-section">
+    //               <h4>Borrow</h4>
+    //               <p>from 1 day to 3 months</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>Period</h4>
+    //               <p>from 1 day to 3 months</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>LTV ratio</h4>
+    //               <p>80%</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>Interest rate</h4>
+    //               <p>1%</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>APR</h4>
+    //               <p>365% - 4%</p>
+    //             </div>
+    //             <div className="block-section">
+    //               <h4>User</h4>
+    //               <p>ObsidianCalyx (No rating)</p>
+    //             </div>
+    //           </div>
+    //           <div className="read-more">
+    //             <p>
+    //               Сlick here for more information{' '}
+    //               <img src="/images/Arrow 3.png" alt="" />
+    //             </p>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 export default Lend;
--- src/pages/Voting/Voting.tsx
+++ src/pages/Voting/Voting.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Voting.css';
 import Mobile from '../NoMobileSupport/Mobile';
+import ComingSoon from '../ComingSoon/ComingSoon';
 // import { withStyles } from '@material-ui/core/styles';
 // import LinearProgress from '@material-ui/core/LinearProgress';
 // const BorderLinearProgress = withStyles((theme) => ({
@@ -28,68 +29,61 @@ const Voting: React.FC<VotingProps> = ({ isMobile }) => {
     return <Mobile />;
   }
   return (
-    <div className="Voting">
-      <div className="header-voting">
-        {/* <h2>Voting</h2>
-        <button>NEXT VOTE</button> */}
-      </div>
-      <h1 style={{ margin: '0px auto', textAlign: 'center', color: 'white' }}>
-        Coming Soon
-      </h1>
-      <img
-        src="/images/illustration2.svg"
-        style={{ width: '100%' }}
-        alt="Coming Soon"
-      />
-      {/* <div className="chips-voting">
-        <div className="chip-voting">
-          Status
-          <img
-            src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
-            alt=""
-          />
-        </div>
-        <div className="chip-voting">
-          Outcome
-          <img
-            src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
-            alt=""
-          />
-        </div>
-        <div className="chip-voting">
-          App
-          <img
-            src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
-            alt=""
-          />
-        </div>
-        <div className="chip-voting">
-          Start day
-          <img src="/images/calendar 1.png" alt="" />
-        </div>
-        <div className="chip-voting">
-          End day
-          <img src="/images/calendar 1.png" alt="" />
-        </div>
-      </div>
-      <div className="section-voting">
-        <h2>Closed voting</h2>
-        <div className="section-cart">
-          <h2>Voting</h2>
-          <p>
-            <span>#0</span> Lorem ipsum dolor sit amet, consec adipiscing elit.
-          </p>
-          <div className="range">
-            <p>Yes</p>
-            <div className="yes">
-              <BorderLinearProgress variant="determinate" value={50} />
-            </div>
-            <p>No</p>
-            <BorderLinearProgress variant="determinate" value={70} />
-          </div>
-        </div>
-      </div> */}
-    </div>
+    <ComingSoon/>
+    // <div className="Voting">
+    //   <div className="header-voting">
+    //     <h2>Voting</h2>
+    //     <button>NEXT VOTE</button>
+    //   </div>
+    //   <div className="chips-voting">
+    //     <div className="chip-voting">
+    //       Status
+    //       <img
+    //         src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
+    //         alt=""
+    //       />
+    //     </div>
+    //     <div className="chip-voting">
+    //       Outcome
+    //       <img
+    //         src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
+    //         alt=""
+    //       />
+    //     </div>
+    //     <div className="chip-voting">
+    //       App
+    //       <img
+    //         src="/images/iconfinder_icon-arrow-down-b_211614 (1) 1.png"
+    //         alt=""
+    //       />
+    //     </div>
+    //     <div className="chip-voting">
+    //       Start day
+    //       <img src="/images/calendar 1.png" alt="" />
+    //     </div>
+    //     <div className="chip-voting">
+    //       End day
+    //       <img src="/images/calendar 1.png" alt="" />
+    //     </div>
+    //   </div>
+    //   <div className="section-voting">
+    //     <h2>Closed voting</h2>
+    //     <div className="section-cart">
+    //       <h2>Voting</h2>
+    //       <p>
+    //         <span>#0</span> Lorem ipsum dolor sit amet, consec adipiscing elit.
+    //       </p>
+    //       <div className="range">
+    //         <p>Yes</p>
+    //         <div className="yes">
+    //           <BorderLinearProgress variant="determinate" value={50} />
+    //         </div>
+    //         <p>No</p>
+    //         <BorderLinearProgress variant="determinate" value={70} />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 export default Voting;
